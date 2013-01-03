dc84dcf5df927374b6b54da540984c0179486efb
Thomas Parslow <tom@almostobsolete.net>
2013-01-03 17:04:31 +0000

    Remove old bits of previous app from admin setup!

--- MYAPPNAME/admin.py
+++ MYAPPNAME/admin.py
@@ -1,21 +1,4 @@
 from django.contrib import admin
 from models import *
 
-class ActionTypeAdmin(admin.ModelAdmin):
-    fieldsets = (
-        (None, {
-            'fields': ('enabled', 'label')
-        }),
-        ('Facebook', {
-            'fields': ('facebook_post', 'message', 'link', 'photo',)
-        }),
-        # ('Advanced', {
-        #     'classes': ('collapse',),
-        #     'fields': ('hook_url',)
-        # }),
-
-    )
-
-admin.site.register(ActionType, ActionTypeAdmin)
-
-admin.site.register(AuthDataWhitelist)
+#admin.site.register(MyModel)
